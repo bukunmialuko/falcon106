b9290a415ad1e79e88390a1dfa1bc475ee25647d
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-07-10 12:23:39 +0100

    Code clean up

--- lib/ui/widgets/cart_item_card.dart
+++ lib/ui/widgets/cart_item_card.dart
@@ -7,17 +7,6 @@ class CartItemCard extends StatelessWidget {
   final Product product;
   final int quantity;
 
-  List<Widget> _getProductDetails(BuildContext context) {
-    return [
-      Text(product.name,
-          maxLines: 2, style: Theme.of(context).textTheme.subtitle),
-      Text('\$${product.price.toString()}.00',
-          style: Theme.of(context).textTheme.title),
-      Text('\Qty: ${quantity.toString()}',
-          style: Theme.of(context).textTheme.subtitle)
-    ];
-  }
-
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -76,36 +65,4 @@ class CartItemCard extends StatelessWidget {
       ),
     );
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Card(
-  //     elevation: 4.0,
-  //     margin: new EdgeInsets.symmetric(horizontal: 20.0, vertical: 6.0),
-  //     child: Container(
-  //       height: 150.0,
-  //       decoration: BoxDecoration(color: Colors.white),
-  //       child: ListTile(
-  //         title: Row(
-  //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //           children: <Widget>[
-  //             Expanded(
-  //               flex: 1,
-  //               child: Padding(
-  //                 padding: const EdgeInsets.only(left: 8.0),
-  //                 child: Column(
-  //                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  //                   crossAxisAlignment: CrossAxisAlignment.start,
-  //                   children: _getProductDetails(context),
-  //                 ),
-  //               ),
-  //             ),
-  //             VerticalDivider(),
-  //             ProductImage(product.imageUrl),
-  //           ],
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
 }
